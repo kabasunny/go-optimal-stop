5c6f8f1fba355ded85a28fd304fa412ad504fda5
kabasunny <dxbqd553@yahoo.co.jp>
2025-01-26 21:26:15 +0900

    パイソンに合わせて修正

--- main.go
+++ main.go
@@ -25,12 +25,12 @@ func main() {
 	if !*useRandom {
 		fmt.Printf("学習モデルのシグナルで検証\n")
 
-		filePath := "data/ml_stock_response/latest_response.bin"
+		filePath := "data/ml_stock_response/proto_kmeans-cluster_label_0.bin"
 		optimization.RunOptimization(filePath, params)
 	} else {
 		fmt.Printf("ランダムにシグナルを作成し、結果を確認\n")
 
-  	// 今日の日付を取得し、フォーマットする
+		// 今日の日付を取得し、フォーマットする
 		today := time.Now().Format("2006-01-02")
 
 		// 予めPython側が、今日の日付で出力している前提
@@ -44,7 +44,7 @@ func main() {
 		}
 		fmt.Printf("Symbols: %v\n", symbols)
 
-		numSignals := 1527
+		numSignals := 16513
 		// フラグを定義
 		useRandomSeed := true // trueはランダム値、falseは固定値
 		attempts := 3         // useRandomSeed := true の時、ランダム値試行を繰り返す回数
